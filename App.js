e643c001387a85f7e59a9259be5167a398b80330
Antonis <anikolaidis@telesto.gr>
2023-09-21 21:12:05 +0300

    Install native base

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./src/navigators/MainNavigator";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </NativeBaseProvider>
     </SafeAreaProvider>
   );
 }
